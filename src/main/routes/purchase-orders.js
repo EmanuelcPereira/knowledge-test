b7e4d0af2abf741609de9408f8d443e640ce3b29
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-09-17 15:37:02 -0300

    feat: added GET purchase order route

--- src/main/routes/purchase-orders.js
+++ src/main/routes/purchase-orders.js
@@ -1,6 +1,8 @@
 const { adaptRoute } = require('../adapters/express-router-adapter');
 const makeCreatePurchaseOrdersController = require('../factories/controllers/purchaseOrders/create-purchase-orders');
+const makeFindAllPurchaseOrdersController = require('../factories/controllers/purchaseOrders/find-all-purchase-orders');
 
 module.exports = (router) => {
+    router.get('/orders', adaptRoute(makeFindAllPurchaseOrdersController()));
     router.post('/orders', adaptRoute(makeCreatePurchaseOrdersController()));
 };
