3a186e476a3e2b361473bc6a54cb354958d0d61d
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-09-17 21:49:44 -0300

    feat: added delete router to purchase Orders

--- src/main/routes/purchase-orders.js
+++ src/main/routes/purchase-orders.js
@@ -1,8 +1,10 @@
 const { adaptRoute } = require('../adapters/express-router-adapter');
 const makeCreatePurchaseOrdersController = require('../factories/controllers/purchaseOrders/create-purchase-orders');
 const makeFindAllPurchaseOrdersController = require('../factories/controllers/purchaseOrders/find-all-purchase-orders');
+const makeDeletePurchaseOrderController = require('../factories/controllers/purchaseOrders/delete-purchase-orders');
 
 module.exports = (router) => {
     router.get('/orders', adaptRoute(makeFindAllPurchaseOrdersController()));
     router.post('/orders', adaptRoute(makeCreatePurchaseOrdersController()));
+    router.delete('/orders/:id', adaptRoute(makeDeletePurchaseOrderController()));
 };
