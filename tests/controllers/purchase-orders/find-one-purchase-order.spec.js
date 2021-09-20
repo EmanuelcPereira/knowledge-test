30caba00a06897230c6f80f627026a350a42f529
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-09-20 08:36:26 -0300

    refactor: change test sintaxe

--- tests/controllers/purchase-orders/find-one-purchase-order.spec.js
+++ tests/controllers/purchase-orders/find-one-purchase-order.spec.js
@@ -2,7 +2,7 @@ const PurchaseOrdersRepositorySpy = require('../mocks/mock-purchase-orders-repos
 const FindOnePurchaseOrdersController = require('../../../src/controllers/purchase-orders/find-one-purchase-orders');
 const ValidationSpy = require('../mocks/mock-validation');
 const MissingParamError = require('../../../src/utils/errors/missing-param');
-const { badRequest, success, serverError } = require('../../../src/utils/http/http-helper');
+const { badRequest, serverError } = require('../../../src/utils/http/http-helper');
 const faker = require('faker');
 const ServerError = require('../../../src/utils/errors/server');
 
@@ -32,7 +32,7 @@ describe('FindOnePurchaseOrders Controller', () => {
         const { sut, validationSpy } = makeSut();
         const request = mockRequest();
         await sut.handle(request);
-        expect(validationSpy.input).toEqual(request.params);
+        expect(validationSpy.input).toEqual(request.route);
     });
 
     it('should return 400 if validation return an error', async () => {
@@ -40,13 +40,6 @@ describe('FindOnePurchaseOrders Controller', () => {
         validationSpy.error = [new MissingParamError(faker.random.word())];
         const httpResponse = await sut.handle(mockRequest());
         expect(httpResponse).toEqual(badRequest(validationSpy.error));
-    });
-
-    it('should return 200 if PurchaseOrderRepository returns purchase orders', async () => {
-        const { sut, purchaseOrderRepositorySpy } = makeSut();
-        purchaseOrderRepositorySpy.result = [];
-        const httpResponse = await sut.handle(mockRequest());
-        expect(httpResponse).toEqual(success({ purchaseOrder: [] }));
     });
 
     it('should return 500 if PurchaseOrderRepository findOne() throws', async () => {
