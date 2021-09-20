832901e2a7c6e3dfe1a2e5a3f3b12a745a954c9f
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-09-20 08:40:55 -0300

    feat: add GET findOne route to purchase orders

--- src/main/routes/purchase-orders.js
+++ src/main/routes/purchase-orders.js
@@ -2,9 +2,11 @@ const { adaptRoute } = require('../adapters/express-router-adapter');
 const makeCreatePurchaseOrdersController = require('../factories/controllers/purchaseOrders/create-purchase-orders');
 const makeFindAllPurchaseOrdersController = require('../factories/controllers/purchaseOrders/find-all-purchase-orders');
 const makeDeletePurchaseOrderController = require('../factories/controllers/purchaseOrders/delete-purchase-orders');
+const makeFindOnePurchaseOrderController = require('../factories/controllers/purchaseOrders/find-one-purchase-orders');
 
 module.exports = (router) => {
     router.get('/orders', adaptRoute(makeFindAllPurchaseOrdersController()));
+    router.get('/orders/:id', adaptRoute(makeFindOnePurchaseOrderController()));
     router.post('/orders', adaptRoute(makeCreatePurchaseOrdersController()));
     router.delete('/orders/:id', adaptRoute(makeDeletePurchaseOrderController()));
 };
