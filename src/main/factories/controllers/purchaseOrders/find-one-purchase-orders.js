c43aaff8ad40e356e8b285a61e8c53c2cbc58b0b
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-09-20 08:40:07 -0300

    feat: add validator to findOne

--- src/main/factories/controllers/purchaseOrders/find-one-purchase-orders.js
+++ src/main/factories/controllers/purchaseOrders/find-one-purchase-orders.js
@@ -1,8 +1,11 @@
 const PurchaseOrdersRepository = require('../../../../repositories/purchase-orders');
 const FindOnePurchaseOrdersController = require('../../../../controllers/purchase-orders/find-one-purchase-orders');
+const makeDeletePurchaseOrderValidators = require('../../validators/purchase-order/delete-purchase-order');
+
 
 module.exports = () => {
     const repository = new PurchaseOrdersRepository();
+    const validators = makeDeletePurchaseOrderValidators();
 
-    return new FindOnePurchaseOrdersController(repository);
+    return new FindOnePurchaseOrdersController(repository, validators);
 };
